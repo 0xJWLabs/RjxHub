6c64a43a2bff569e85370c42a02923c4a75f5692
0xJWLabs <0xjwlabs@gmail.com>
2025-03-10 17:36:54 +0700

    chore: initial commit

---
+++ loader.lua
@@ -0,0 +1,44 @@
+local function clone<Original>(ToClone: any & Original): Original
+	if cloneref or clonereference then
+		return ToClone
+	end
+	
+	return ToClone
+end
+
+local HttpService = clone(game:GetService("HttpService"))
+local Url = "https://raw.githubusercontent.com/0xJWLabs/RjxHub/refs/heads/main/index.json"
+
+local Success, Response = pcall(function()
+    return game:HttpGet(Url)
+end)
+
+if Success then
+    local Data = HttpService:JSONDecode(Response)
+    local PlaceId = tostring(game.PlaceId)
+    
+    if Data.games and Data.games[PlaceId] then
+        local ScriptUrl = Data.games[PlaceId]
+        print("Loading Script For Game ID:", PlaceId)
+        
+        local ScriptSuccess, ScriptResponse = pcall(function()
+            return game:HttpGet(ScriptUrl)
+        end)
+        
+        if ScriptSuccess then
+            local LoadSuccess, ErrorMsg = pcall(function()
+                loadstring(ScriptResponse)()
+            end)
+            
+            if not LoadSuccess then
+                warn("Error Executing Script:", ErrorMsg)
+            end
+        else
+            warn("Failed To Fetch Script From:", ScriptUrl)
+        end
+    else
+        warn("No Supported Script Found For This Game (PlaceId:", PlaceId, ")")
+    end
+else
+    warn("Failed To Fetch Game Script Index From:", Url)
+end
